10d79be866e36e091d84ea8ad559bd062eff4e49
LenHsu <110470216+len0529@users.noreply.github.com>
2023-06-01 18:22:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal({
+    distance:'50px',
+    duration: 2600,
+    reset:false
+})
+sr.reveal('.main-title',{delay:100,origin:'top'})
+sr.reveal('.about-text',{delay:100,origin:'right'})
+sr.reveal('.intro-col',{delay:50,origin:'top'})
+sr.reveal('.offer-content',{delay:40,origin:'top'})
+sr.reveal('.textandphoto',{delay:200,origin:'right'})
+sr.reveal('.vision-1',{delay:200,origin:'top'})
+sr.reveal('.vision-text',{delay:200,origin:'left'})
+sr.reveal('.vision-img',{delay:200,origin:'right'})
+
+
